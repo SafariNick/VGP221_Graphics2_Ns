0842db1fa78bd867bc11d56afd99a8b47316c63c
SafariNick <nick.oam@hotmail.com>
2026-05-07 22:58:44 -0400

    Map Update

--- Source/VGPSping2026/VGPSping2026.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VGPSping2026.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VGPSping2026, "VGPSping2026" );
